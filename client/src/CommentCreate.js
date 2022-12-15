480837b3ade20708a6357c3a22d993991a680600
velopace <velopace@outlook.com>
2022-12-15 10:50:39 -0600

    comments

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent('');
+  };
+
+  return (
+    <div>
+      <form className="row g-3" onSubmit={onSubmit}>
+        <div className="col-12">
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="col-12">
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};

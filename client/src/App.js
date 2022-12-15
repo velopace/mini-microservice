b88983dd3f70549cbfd0028faa9b268af40f6a73
velopace <velopace@outlook.com>
2022-12-15 10:17:58 -0600

    fix cors errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,10 @@
+import PostCreate from './PostCreate';
+
 export default () => {
-  return <div>Blog app</div>;
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
---
+++ client/src/PostCreate.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', { title });
+
+    setTitle('');
+  };
+
+  return (
+    <form className="row g-3" onSubmit={onSubmit}>
+      <div className="col-12">
+        <label>Title</label>
+        <input
+          value={title}
+          className="form-control"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="col-12">
+        <button className="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  );
+};
